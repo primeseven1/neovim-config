6bc7bfc06f02b83abdfef7f6dfef3541bfb2b34d
Christopher Fandrich <cgfcubing@gmail.com>
2024-01-28 22:10:17 -0800

    Add autopairs

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,5 +1,11 @@
 local function do_neotree_config()
     vim.keymap.set('n', "<C-e>", ":Neotree filesystem reveal left<CR>", {})
+
+    vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+    vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+    vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
     require("neo-tree").setup {
         filesystem = {
             filtered_items = {
@@ -7,7 +13,20 @@ local function do_neotree_config()
                 hide_dotfiles = false,
                 hide_gitignored = false,
             }
-        }
+        },
+        git_status = {
+            symbols = {
+                added = "✚",
+                modified = "",
+                deleted = "✖",
+                renamed = "󰁕",
+                untracked = "",
+                ignored = "",
+                unstaged = "󰄱",
+                staged = "",
+                conflict = "",
+            }
+        },
     }
 end
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 local function do_treesitter_config()
     local config = require("nvim-treesitter.configs")
     config.setup({
-        ensure_installed = { 
-            "bash", "c", "c_sharp", "cmake", "cpp", "css", "html", "ini", "java", "javascript", "json", 
+        ensure_installed = {
+            "bash", "c", "c_sharp", "cmake", "cpp", "css", "html", "ini", "java", "javascript", "json",
             "linkerscript", "lua", "luau", "make", "meson", "nasm", "python", "rust", "xml", "yaml"
         }
     })
