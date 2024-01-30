9604d3e5e427487fd8dc525eff5ae61dfb56787f
Christopher Fandrich <cgfcubing@gmail.com>
2024-01-30 03:57:26 -0800

    Hide gitignored files in neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -9,9 +9,9 @@ local function do_neotree_config()
     require("neo-tree").setup {
         filesystem = {
             filtered_items = {
-                visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+                visible = false, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
                 hide_dotfiles = false,
-                hide_gitignored = false,
+                hide_gitignored = true,
             }
         },
         git_status = {
